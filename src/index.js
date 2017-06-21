c02461feddf6b5e947ea4f169fd7ee3d1df00e0f
jesse <jsoldat@hotmail.com>
2017-06-21 18:00:29 -0400

    created store and reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reducers from './reducers';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+	<App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
